a3bc58aea5833f021cf8ac06fac7df42b1a7077a
Ryabova Veronika IU5-55B <70639339+nika-rvv@users.noreply.github.com>
2022-06-02 21:14:49 +0300

    нир

---
+++ Main View/MainViewController.swift
@@ -0,0 +1,105 @@
+
+import UIKit
+
+class MainViewController: UIViewController {
+    var firstRun = true
+
+    let imagePredictor = ImagePredictor()
+
+    let predictionsToShow = 2
+
+    // MARK: Main storyboard outlets
+    @IBOutlet weak var startupPrompts: UIStackView!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var predictionLabel: UILabel!
+}
+
+extension MainViewController {
+    // MARK: Main storyboard actions
+    @IBAction func singleTap() {
+        guard UIImagePickerController.isSourceTypeAvailable(.camera) else {
+            present(photoPicker, animated: false)
+            return
+        }
+
+        present(cameraPicker, animated: false)
+    }
+
+    @IBAction func doubleTap() {
+        present(self.photoPicker, animated: false)
+    }
+}
+
+extension MainViewController {
+    // MARK: Main storyboard updates
+    func updateImage(_ image: UIImage) {
+        DispatchQueue.main.async {
+            self.imageView.image = image
+        }
+    }
+    /// - Parameter message
+
+    func updatePredictionLabel(_ message: String) {
+        DispatchQueue.main.async {
+            self.predictionLabel.text = message
+        }
+
+        if firstRun {
+            DispatchQueue.main.async {
+                self.firstRun = false
+                self.predictionLabel.superview?.isHidden = false
+                self.startupPrompts.isHidden = true
+            }
+        }
+    }
+    /// - Parameter photo
+    func userSelectedPhoto(_ photo: UIImage) {
+        updateImage(photo)
+        updatePredictionLabel("Making predictions for the photo...")
+
+        DispatchQueue.global(qos: .userInitiated).async {
+            self.classifyImage(photo)
+        }
+    }
+
+}
+
+extension MainViewController {
+    // MARK: Image prediction methods
+    private func classifyImage(_ image: UIImage) {
+        do {
+            try self.imagePredictor.makePredictions(for: image,
+                                                    completionHandler: imagePredictionHandler)
+        } catch {
+            print("Vision was unable to make a prediction...\n\n\(error.localizedDescription)")
+        }
+    }
+
+    private func imagePredictionHandler(_ predictions: [ImagePredictor.Prediction]?) {
+        guard let predictions = predictions else {
+            updatePredictionLabel("No predictions. (Check console log.)")
+            return
+        }
+
+        let formattedPredictions = formatPredictions(predictions)
+
+        let predictionString = formattedPredictions.joined(separator: "\n")
+        updatePredictionLabel(predictionString)
+    }
+
+    private func formatPredictions(_ predictions: [ImagePredictor.Prediction]) -> [String] {
+        // Vision sorts the classifications in descending confidence order.
+        let topPredictions: [String] = predictions.prefix(predictionsToShow).map { prediction in
+            var name = prediction.classification
+
+            // For classifications with more than one name, keep the one before the first comma.
+            if let firstComma = name.firstIndex(of: ",") {
+                name = String(name.prefix(upTo: firstComma))
+            }
+
+            return "\(name) - \(prediction.confidencePercentage)%"
+        }
+
+        return topPredictions
+    }
+}
